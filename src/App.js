458501e02810fc670ed5498a06165d9dc12e4149
damianjnc <damianjnc@gmail.com>
2018-11-20 04:31:18 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-    constructor(props){
-      super(props);
-      this.state = {manager:''};
-    }
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
 
     async componentDidMount(){
       const manager = await lottery.methods.manager().call();
-      this.setState({manager});
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+
+      this.setState({manager, players, balance});
     }
 
 
@@ -21,7 +26,23 @@ class App extends Component {
 
     return (
       <div><h2>Lottery Contract</h2>
-      <p>This contract is managed by {this.state.manager}</p>
+      <p>This contract is managed by {this.state.manager}.
+      There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+
+      <hr />
+
+      <form>
+        <h4>Want to try your luck?        </h4>
+          <div>
+              <label> Amount of ether to enter: </label>
+                <input
+                  value = {this.state.value}
+                  onChange = {event => this.setState({value: event.target.value})}
+                  />
+
+          </div>
+            <button>Enter</button>
+      </form>
       </div>
 
     );
