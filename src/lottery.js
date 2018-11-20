89eb6c7762fcbd404df042ff850244113fda2ec2
damianjnc <damianjnc@gmail.com>
2018-11-20 03:35:15 +0000

    second commit

--- src/lottery.js
+++ src/lottery.js
@@ -1,12 +1,14 @@
-import web3 from 'web3';
+import web3 from './web3';
 
-const address = '0x693d9a2740aD1d02e17059f21a7FD095CeF6B0A9';
-const abi = '[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability
-":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutabil
-ity":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"addr
-ess[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outpu
-ts":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint2
-56"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"functio
-n"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]';
+const address = '0x199F5cAB385374f8007D0a34E9308c9Fd69A68F9';
+const abi =
+[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,
+"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,
+
+"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":
+"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter",
+"outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":
+"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":
+"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi, address);
